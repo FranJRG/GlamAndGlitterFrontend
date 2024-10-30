2e4970b90047735095e21e690227c3779ec3397a
FranJRG <framirezgarcia0@gmail.com>
2024-10-30 23:51:23 +0100

    loader

--- src/app/shared/interceptors/jwt.interceptor.ts
+++ src/app/shared/interceptors/jwt.interceptor.ts
@@ -1,12 +1,18 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { finalize } from 'rxjs';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
+  const loader = inject(NgxUiLoaderService);
+
+  loader.start();
   if (token) {
     //Si existe
     req = req.clone({
       setHeaders:{Authorization:token} //Lo añadimos a la cabecera
     });
   }
-  return next(req);
+  return next(req).pipe(finalize(() => loader.stop()));
 };
